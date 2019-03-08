5c2d79dddd940e43380ad56e2bb6ba6ca360f00a
Dylan Roberts <DylanRoberts@dal.ca>
2019-03-08 19:49:42 -0400

    made some progress with the bracket stuff

--- A3PPL/src/Scanner2point0.java
+++ A3PPL/src/Scanner2point0.java
@@ -18,8 +18,10 @@ public class Scanner2point0 {
 	
 	public static ArrayList <ArrayList<String>> makeTokens(ArrayList<String> input){
 		ArrayList <ArrayList<String>> tokens = new ArrayList <ArrayList <String>>();
-		
 		for(int line = 0; line < input.size(); line++){
+			ArrayList<String> tempToken = new ArrayList <String>();
+			char[] toke = new char[100] ;
+			int tokeCount = 0;
 			int posiCharacter = 1;
 			for(int currCharacter = 0; currCharacter < input.get(line).length(); posiCharacter++, currCharacter++){
 				if( input.get(line).charAt(currCharacter) ==  '(' ||
@@ -28,14 +30,25 @@ public class Scanner2point0 {
 					input.get(line).charAt(currCharacter) ==  ']' ||
 					input.get(line).charAt(currCharacter) ==  '{' ||
 					input.get(line).charAt(currCharacter) ==  '}') {
-					
-					
+					tempToken.add(parenthCall(input.get(line).charAt(currCharacter)));
+					tempToken.add(Integer.toString(line));
+					tempToken.add(Integer.toString(posiCharacter));
+					tokens.add(tempToken);
+					tempToken.clear();
 				}
 				else if(input.get(line).charAt(currCharacter) == '"'){
-					
+					//TODO 
 				}
 				else if(input.get(line).charAt(currCharacter) == ' ' ){
-					
+					if (toke[0] == '\0')
+						continue;
+					else{
+						indentToken(toke);
+						toke
+						
+					}
+						
+						
 				}else if(input.get(line).charAt(currCharacter) == '\t'){
 					
 				}else{
