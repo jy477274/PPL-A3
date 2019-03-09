68e3a0eaf17198d31e9a0db42917948e5af3630e
Jonathan Cookson <jonathan.cookson@dal.ca>
2019-03-09 18:35:04 -0400

    Switch Case stuff

--- A3PPL/src/Scanner2point0.java
+++ A3PPL/src/Scanner2point0.java
@@ -30,6 +30,17 @@ public class Scanner2point0 {
 					input.get(line).charAt(currCharacter) ==  ']' ||
 					input.get(line).charAt(currCharacter) ==  '{' ||
 					input.get(line).charAt(currCharacter) ==  '}') {
+					
+					if (! (toke[0] == '\0')) {
+						String token = new String(toke).trim().replaceAll(" ",  "");
+						Arrays.fill(toke, '\0');
+						
+						tempToken.add(identToken(token));
+						tempToken.add(Integer.toString(line));
+						tempToken.add(Integer.toString(posiCharacter));
+						tempToken.clear();
+					}
+					
 					tempToken.add(parenthCall(input.get(line).charAt(currCharacter)));
 					tempToken.add(Integer.toString(line));
 					tempToken.add(Integer.toString(posiCharacter));
@@ -43,81 +54,114 @@ public class Scanner2point0 {
 					if (toke[0] == '\0')
 						continue;
 					else{
-						indentToken(toke);
+						String token = new String(toke).trim().replaceAll(" ",  "");
 						Arrays.fill(toke, '\0');
+						
+						tempToken.add(identToken(token));
+						tempToken.add(Integer.toString(line));
+						tempToken.add(Integer.toString(posiCharacter));
+						tempToken.clear();
 					}
 						
 						
 				}else if(input.get(line).charAt(currCharacter) == '\t'){
 					
 				}else{
-					identToken(toke);
+					String token = new String(toke).trim().replaceAll(" ",  "");
+					Arrays.fill(toke, '\0');
+					
+					tempToken.add(identToken(token));
+					tempToken.add(Integer.toString(line));
+					tempToken.add(Integer.toString(posiCharacter));
+					tempToken.clear();
 				}
 			}
 		}	
 		return tokens; 
 	}
 	
-	public static String identToken(char [] c){
+	public static String identToken(String s){
 		String tokenType = null;
-		switch(c[0]){
+		switch(s.charAt(0)){
 		case '+':
 			tokenType = numberToken();
+			break;
 		case '-':
 			tokenType = numberToken();
+			break;
 		case '0':
 			tokenType = numberToken();
+			break;
 		case '1':
 			tokenType = numberToken();
+			break;
 		case '2':
 			tokenType = numberToken();
+			break;
 		case '3':
 			tokenType = numberToken();
+			break;
 		case '4':
 			tokenType = numberToken();
+			break;
 		case '5':
 			tokenType = numberToken();
+			break;
 		case '6':
 			tokenType = numberToken();
+			break;
 		case '7':
 			tokenType = numberToken();
+			break;
 		case '8':
 			tokenType = numberToken();
+			break;
 		case '9':
 			tokenType = numberToken();
+			break;
 		case '#':
-			if(c[1] =='t' || c[1] =='f')
-				tokenType = boolToken(c);
+			if(s.charAt(1) =='t' || s.charAt(1) =='f')
+				tokenType = boolToken(s);
 			else
-				tokenType = charToken(c);
+				tokenType = charToken(s);
+			break;
+		default :
+			break;
 		}
 		
 		
 		return tokenType;
 	}
 	
-	private static String charToken(char[] c) {
-		if(Arrays.equals(c, new char [] {'#','\\','\\','n','e','w','l','i','n','e'})){
-			
-		}else if(Arrays.equals(c, new char [] {'#','\\','\\','s','p','a','c','e'})){
-			
-		}else if(Arrays.equals(c, new char [] {'#','\\','\\','t','a','b'})){
-		}else if(ANY CHAR){
-			
+	private static String charToken(String s) {
+		
+		switch(s) {
+		case "#\\\\newline" :
+			return "CHAR";
+		case "#\\\\space" :
+			return "CHAR";
+		case "#\\\\tab" : 
+			return "CHAR";
+		case " " :
+			return "CHAR";
+		case "  " :
+			return "CHAR";
+		default :
+			return "ERROR";
 		}
-		else if(ANY CHAR ident by 3 dig octal){
-			
-		}
-		return null;
+		
 	}
 
-	private static String boolToken(char [] c) {
-		if(c[1] =='t')
+	private static String boolToken(String s) {
+		
+		switch(s.charAt(1)) {
+		case 't' :
 			return "BOOL";
-		else if(c[1] =='f')
+		case 'f' :
 			return "BOOL";
-		else
+		default :
 			return "ERROR";
+		}
 	}
 
 	public static String numberToken() {
