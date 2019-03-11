b99674e14c336726bcfb18a427e5f4dee42aa8de
Dylan Roberts <DylanRoberts@dal.ca>
2019-03-11 16:09:17 -0300

    added half working float stuff, and probably working bi and hex and signed dec stuff

--- A3PPL/src/Scanner2point0.java
+++ A3PPL/src/Scanner2point0.java
@@ -83,19 +83,19 @@ public class Scanner2point0 {
 	public static String identToken(String s){
 		switch(s){
 		case "lambda" :
-			return "lambda";
+			return "LAMBDA";
 		case "define" :
-			return "define";
+			return "DEFINE";
 		case "let" :
-			return "let";
+			return "LET";
 		case "cond" :
-			return "cond";
+			return "COND";
 		case "if" :
-			return "if";
+			return "IF";
 		case "begin" :
-			return "begin";
+			return "BEGIN";
 		case "quote" :
-			return "quote";
+			return "QUOTE";
 		default :
 			break;
 		}
@@ -139,30 +139,30 @@ public class Scanner2point0 {
 		
 		switch(s.charAt(0)){
 		case '+' :
-			return numberToken();
+			return numberToken(s);
 		case '-' :
-			return numberToken();
+			return numberToken(s);
 		case '0' :
-			return numberToken();
+			return numberToken(s);
 		case '1' :
-			return numberToken();
+			return numberToken(s);
 		case '2' :
-			return numberToken();
+			return numberToken(s);
 		case '3' :
-			return numberToken();
+			return numberToken(s);
 		case '4' :
-			return numberToken();
+			return numberToken(s);
 		case '5' :
-			return numberToken();
+			return numberToken(s);
 		case '6' :
-			return numberToken();
+			return numberToken(s);
 		case '7' :
-			return numberToken();
+			return numberToken(s);
 		case '8' :
-			return numberToken();
+			return numberToken(s);
 
 		case '9' :
-			return numberToken();
+			return numberToken(s);
 		case '#' :
 			if(s.charAt(1) =='t' || s.charAt(1) =='f')
 				return boolToken(s);
@@ -210,11 +210,117 @@ public class Scanner2point0 {
 		}
 	}
 
-	public static String numberToken() {
+	public static String numberToken(String s) {
+		s.toLowerCase();
+		if(s.startsWith("0b")){
+			if(!isBi(s.substring(2, s.length())))
+				return "ERROR";
 		
-		return null;
+		}else if(s.startsWith("0x")){
+			if(!isHex(s.substring(2, s.length())))
+				return "ERROR";
+		
+		}else if(s.charAt(0) == '+' || s.charAt(0) == '-' || isNum(s.substring(0,1))){
+			if(s.charAt(0) != '+' && s.charAt(0) != '-'){
+				if(isNum(s))// if s is a decimal signed integer we know it is a number
+					return "NUMBER";
+				else if(s.contains(".") && isFloatPoint(s)){//if s contains a '.' we know that it must fit the regular expression that contains a .
+					return "NUMBER";
+				}else if(!s.contains(".") && isFloatNoPoint(s)){//if s does not contain a '.' then it has to satisfy the reg expression that only contains eE 
+					return "NUMBER";
+				}
+				
+			}else if(s.charAt(0) == '+' || s.charAt(0) == '-'){//if s has a + or minus sign we can use the same methods if we start at char@ 1 on instead of 0 on
+				if(isNum(s.substring(1)))
+					return "NUMBER";
+				else if(s.contains(".") && isFloatPoint(s.substring(1))){
+					return "NUMBER";
+				}else if(!s.contains(".") && isFloatNoPoint(s.substring(1))){
+					return "NUMBER";
+				}	
+			}
+		}
+				
+		return "ERROR";
+	}
+	
+	private static boolean isFloatNoPoint(String s) {//we know that s must have [0-9]+[e(we use tolowercase)][+-]?[0-9}
+		int eValue = s.indexOf('e');
+		if(isNum(s.substring(0, s.charAt(eValue)))){
+			if(s.charAt(eValue+1) == '+' || s.charAt(eValue+1) == '+'){
+				
+			}else{
+				
+			}
+			
+		}
+		
+		return false;
 	}
 
+	public static boolean isFloatPoint(String s) {
+		boolean pointFlag = false;
+		int pointValue = s.indexOf('.') ,count = 0;
+		if(isNum((String) s.subSequence(0, pointValue))){
+			
+		}
+		return false;
+	}
+
+	public static Boolean isBi(String s){
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) != '0' && s.charAt(i) != '1'){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static Boolean isHex(String s){
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) != '0' &&
+				s.charAt(i) != '1' &&
+				s.charAt(i) != '2' &&
+				s.charAt(i) != '3' &&
+				s.charAt(i) != '4' &&
+				s.charAt(i) != '5' &&
+				s.charAt(i) != '6' &&
+				s.charAt(i) != '7' &&
+				s.charAt(i) != '8' &&
+				s.charAt(i) != '9' &&
+				s.charAt(i) != 'A' &&
+				s.charAt(i) != 'B' &&
+				s.charAt(i) != 'C' &&
+				s.charAt(i) != 'D' &&
+				s.charAt(i) != 'E' &&
+				s.charAt(i) != 'F' &&
+				s.charAt(i) != 'a' &&
+				s.charAt(i) != 'b' &&
+				s.charAt(i) != 'c' &&
+				s.charAt(i) != 'e' &&
+				s.charAt(i) != 'f' &&
+				s.charAt(i) != 'd'){
+				return false;
+			}
+		}		
+		return true;		
+	}
+	public static Boolean isNum(String s){
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) != '0' &&
+				s.charAt(i) != '1' &&
+				s.charAt(i) != '2' &&
+				s.charAt(i) != '3' &&
+				s.charAt(i) != '4' &&
+				s.charAt(i) != '5' &&
+				s.charAt(i) != '6' &&
+				s.charAt(i) != '7' &&
+				s.charAt(i) != '8' &&
+				s.charAt(i) != '9')
+				return false;
+		}		
+		return true;	
+	}
 	public static String parenthCall(char c){
 		if( c ==  '(')
 			return "OPENRD";
